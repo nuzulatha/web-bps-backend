273205da9cc0ae95aa62fab949347a67fd81493b
nuzulatha <222313305@stis.ac.id>
2025-07-13 20:33:59 +0700

    First Commit

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,34 @@
 <?php
+// return [
+//     'paths' => ['api/*'],
+//     'allowed_methods' => ['*'],
+//     'allowed_origins' => ['http://localhost:5173', 'https://web-bps-frontend-v5th.vercel.app'],
+//     'allowed_origins_patterns' => [],
+//     'allowed_headers' => ['*'],
+//     'exposed_headers' => [],
+//     'max_age' => 0,
+//     'supports_credentials' => true,
+// ];
 return [
-    'paths' => ['api/*'],
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'https://web-bps-frontend-v5th.vercel.app'],
+
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://web-bps-frontend-v5th.vercel.app',
+    ],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    'supports_credentials' => true,
+
+    'supports_credentials' => false,
 ];
+
 ?>
