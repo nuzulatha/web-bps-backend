edc6a7313c3f6561f4657e86692f703be3c686d8
nuzulatha <222313305@stis.ac.id>
2025-07-14 07:48:54 +0700

    First Commit

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -15,7 +15,6 @@ class SecurityHeaders
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('X-Frame-Options', 'DENY');
         $response->headers->set('X-XSS-Protection', '1; mode=block');
-        $response->headers->remove('X-Powered-By');
         
         return $response;
     }
